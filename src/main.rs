a10fa65f546f322b371025bb6361e19eec0ceca4
Emil Nordén <emilnorden@yahoo.se>
2021-01-02 19:05:06 +0100

    moved some window related code around

--- src/main.rs
+++ src/main.rs
@@ -86,78 +86,7 @@ fn main() {
     // camera.set_direction(glm::Vector3::new(0.0, -1.0, -1.0));
     camera.set_resolution(glm::Vector2::new(window.width(), window.height()));
 
-    use std::ffi::CString;
-    let vert_shader = render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap()).unwrap();
-    let frag_shader = render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap()).unwrap();
 
-    let shader_program = render_gl::Program::from_shaders(
-        &[vert_shader, frag_shader]
-    ).unwrap();
-
-    let vertices: Vec<f32> = vec![
-        -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, // uppe vänster?
-        1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, // uppe höger
-        1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, // nere höger
-
-        -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, // uppe vänster?
-        1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, // nere höger
-        -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, // nere vänster
-    ];
-
-    let mut vbo: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut vbo);
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-            vertices.as_ptr() as *const gl::types::GLvoid,
-            gl::STATIC_DRAW,
-        );
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-    }
-
-    let mut vao: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-
-        gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            0, // index of the generic vertex attribute ("layout (location = 0)")
-            3, // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            std::ptr::null(), // offset of the first component
-        );
-
-        gl::EnableVertexAttribArray(1); // this is "layout (location = 1)" in vertex shader
-        gl::VertexAttribPointer(
-            1, // index of the generic vertex attribute ("layout (location = 1)")
-            3, // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
-        );
-
-        gl::EnableVertexAttribArray(2); // this is "layout (location = 2)" in vertex shader
-        gl::VertexAttribPointer(
-            2, // index of the generic vertex attribute ("layout (location = 1)")
-            2, // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            (6 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
-        );
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
-    }
     let mut pixels = vec![0u8; (window.width() * window.height() * 3) as usize]; // Vec::<u8>::with_capacity((window.width() * window.height() * 3) as usize);
 
     let texture = Texture::from_pixels(window.width(), window.height(), &pixels).unwrap();
@@ -204,16 +133,7 @@ fn main() {
 
         texture.set_pixels(window.width(), window.height(), &pixels);
         texture.bind();
-
-        shader_program.set_used();
-        unsafe {
-            gl::BindVertexArray(vao);
-            gl::DrawArrays(
-                gl::TRIANGLES,
-                0,
-                6,
-            );
-        }
+        window.render();
         window.swap();
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,8 +1,13 @@
 use sdl2::video::GLContext;
+use crate::render_gl;
+use crate::render_gl::Program;
 
 pub struct Window {
     window: sdl2::video::Window,
     _context: GLContext,
+    vertex_array_object: gl::types::GLuint,
+    vertex_buffer_object: gl::types::GLuint,
+    shader_program: Program,
 }
 
 impl Window {
@@ -34,10 +39,89 @@ impl Window {
 
         sdl_window.set_size(512, 512).unwrap();
 
+        use std::ffi::CString;
+        let vert_shader = render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap()).unwrap();
+        let frag_shader = render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap()).unwrap();
+
+        let shader_program = render_gl::Program::from_shaders(
+            &[vert_shader, frag_shader]
+        ).unwrap();
+
+
+        // Set up full screen quad
+        let vertices: Vec<f32> = vec![
+            -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, // uppe vänster?
+            1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, // uppe höger
+            1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, // nere höger
+
+            -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, // uppe vänster?
+            1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, // nere höger
+            -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, // nere vänster
+        ];
+
+        let mut vbo: gl::types::GLuint = 0;
+        unsafe {
+            gl::GenBuffers(1, &mut vbo);
+
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+                vertices.as_ptr() as *const gl::types::GLvoid,
+                gl::STATIC_DRAW,
+            );
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        }
+
+        let mut vao: gl::types::GLuint = 0;
+        unsafe {
+            gl::GenVertexArrays(1, &mut vao);
+
+            gl::BindVertexArray(vao);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+            gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+            gl::VertexAttribPointer(
+                0, // index of the generic vertex attribute ("layout (location = 0)")
+                3, // the number of components per generic vertex attribute
+                gl::FLOAT, // data type
+                gl::FALSE, // normalized (int-to-float conversion)
+                (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+                std::ptr::null(), // offset of the first component
+            );
+
+            gl::EnableVertexAttribArray(1); // this is "layout (location = 1)" in vertex shader
+            gl::VertexAttribPointer(
+                1, // index of the generic vertex attribute ("layout (location = 1)")
+                3, // the number of components per generic vertex attribute
+                gl::FLOAT, // data type
+                gl::FALSE, // normalized (int-to-float conversion)
+                (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+                (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
+            );
+
+            gl::EnableVertexAttribArray(2); // this is "layout (location = 2)" in vertex shader
+            gl::VertexAttribPointer(
+                2, // index of the generic vertex attribute ("layout (location = 1)")
+                2, // the number of components per generic vertex attribute
+                gl::FLOAT, // data type
+                gl::FALSE, // normalized (int-to-float conversion)
+                (8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+                (6 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
+            );
+
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::BindVertexArray(0);
+        }
+
         let result = Window {
             window: sdl_window,
-            _context: gl_context
+            _context: gl_context,
+            vertex_array_object: vao,
+            vertex_buffer_object: vbo,
+            shader_program,
         };
+
         Ok(result)
     }
 
@@ -56,7 +140,19 @@ impl Window {
     pub fn swap(&self) {
         self.window.gl_swap_window();
     }
+    pub fn render(&self) {
 
+        self.shader_program.set_used();
+
+        unsafe {
+            gl::BindVertexArray(self.vertex_array_object);
+            gl::DrawArrays(
+                gl::TRIANGLES,
+                0,
+                6,
+            );
+        }
+    }
     pub fn clear(&self) {
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
