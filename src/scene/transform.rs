302eafcfef1f9e47c8c3acbe0f298491497464e5
Emil Nordén <emilnorden@yahoo.se>
2021-04-10 20:50:49 +0200

    fixed error in order of transformations

--- src/scene/transform.rs
+++ src/scene/transform.rs
@@ -47,11 +47,64 @@ impl Transform {
     }
 
     fn build_transform(&mut self) {
-        let mut transform = glm::ext::scale(&glm::Mat4::one(), self.scale);
+        let mut transform = glm::ext::translate(&glm::Mat4::one(), self.translation);
         transform = glm::ext::rotate(&transform, self.rotation.z, glm::vec3(0.0, 0.0, 1.0));
         transform = glm::ext::rotate(&transform, self.rotation.y, glm::vec3(0.0, 1.0, 0.0));
         transform = glm::ext::rotate(&transform, self.rotation.x, glm::vec3(1.0, 0.0, 0.0));
-        self.world = glm::ext::translate(&transform, self.translation);
+        self.world = glm::ext::scale(&transform, self.scale);
         self.inverse_world = glm::inverse(&self.world);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn set_translation_should_create_valid_world() {
+        let mut transform = Transform::new();
+        transform.set_translation(glm::vec3(100.0, 200.0, 300.0));
+
+        let result = transform.world();
+        assert_eq!(result.c0, glm::vec4(1.0, 0.0, 0.0, 0.0));
+        assert_eq!(result.c1, glm::vec4(0.0, 1.0, 0.0, 0.0));
+        assert_eq!(result.c2, glm::vec4(0.0, 0.0, 1.0, 0.0));
+        assert_eq!(result.c3, glm::vec4(100.0, 200.0, 300.0, 1.0));
+    }
+
+    #[test]
+    fn set_scale_should_create_valid_world() {
+        let mut transform = Transform::new();
+        transform.set_scale(glm::vec3(100.0, 200.0, 300.0));
+
+        let result = transform.world();
+        assert_eq!(result.c0, glm::vec4(100.0, 0.0, 0.0, 0.0));
+        assert_eq!(result.c1, glm::vec4(0.0, 200.0, 0.0, 0.0));
+        assert_eq!(result.c2, glm::vec4(0.0, 0.0, 300.0, 0.0));
+        assert_eq!(result.c3, glm::vec4(0.0, 0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn composed_transform_should_create_valid_world() {
+        let mut transform = Transform::new();
+        transform.set_translation(glm::vec3(100.0, 200.0, 300.0));
+        transform.set_scale(glm::vec3(4.0, 3.0, 2.0));
+
+        let result = transform.world();
+        assert_eq!(result.c0, glm::vec4(4.0, 0.0, 0.0, 0.0));
+        assert_eq!(result.c1, glm::vec4(0.0, 3.0, 0.0, 0.0));
+        assert_eq!(result.c2, glm::vec4(0.0, 0.0, 2.0, 0.0));
+        assert_eq!(result.c3, glm::vec4(100.0, 200.0, 300.0, 1.0));
+    }
+
+    #[test]
+    fn new_transform_should_equal_identity() {
+        let mut transform = Transform::new();
+
+        let result = transform.world();
+        assert_eq!(result.c0, glm::vec4(1.0, 0.0, 0.0, 0.0));
+        assert_eq!(result.c1, glm::vec4(0.0, 1.0, 0.0, 0.0));
+        assert_eq!(result.c2, glm::vec4(0.0, 0.0, 1.0, 0.0));
+        assert_eq!(result.c3, glm::vec4(0.0, 0.0, 0.0, 1.0));
     }
 }
